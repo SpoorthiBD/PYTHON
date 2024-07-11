7fd30fd845095f74ab602a90571fb5dc195988d0
spoorthibs <spoorthibs2201@gmail.com>
2024-07-11 17:34:37 +0530

    added

--- main2.py
+++ main2.py
@@ -130,4 +130,4 @@ def stop_webcam():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0',port=5000)
+    app.run(debug=True)
